a4fe55790a55bff659d478784acaf5a18e4b94e1
Adrien Rossato <adrien.marcos@gmail.com>
2023-01-19 10:29:17 -0300

    chore: add ignore paths to jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  // collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '<rootDir>/src/presentation/protocols/index.ts'
+  ],
   coverageDirectory: 'coverage',
-  // coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
